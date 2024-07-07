3e00085e31974cc61fc8bc75bda920b03bd5370d
Muhammed Javad Kasaragod <javadjazz899@gmail.com>
2024-07-07 16:52:28 +0530

    multi storage into ts

--- helper/multerStorage.js
+++ helper/multerStorage.js
@@ -1,28 +1,30 @@
-const multer = require("multer");
-const { organizationFileName } = require("./utilHelper");
-
-
-let organizationStorage = {
-
-    kycMulter: multer.diskStorage({
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const multer_1 = __importDefault(require("multer"));
+const utilHelper_1 = __importDefault(require("./utilHelper"));
+const { organizationFileName } = utilHelper_1.default;
+const organizationStorage = {
+    kycMulter: multer_1.default.diskStorage({
         destination: (req, file, cb) => {
             cb(null, 'public/organization/kyc');
         },
         filename: (req, file, cb) => {
-            let file_name = organizationFileName("kyc_", file.filename)
+            let file_name = organizationFileName("kyc_", file.filename);
             cb(null, file_name);
         }
     }),
-
-    logoPicture: multer.diskStorage({
+    logoPicture: multer_1.default.diskStorage({
         destination: (req, file, cb) => {
             cb(null, 'public/organization/logo');
         },
         filename: (req, file, cb) => {
-            let fileName = organizationFileName("logo_", file.filename)
+            let fileName = organizationFileName("logo_", file.filename);
             cb(null, fileName);
         }
     })
-}
-
-module.exports = organizationStorage;
+};
+// module.exports = organizationStorage;
+exports.default = organizationStorage;
