75dcb55b507a4012d7599b0ca26f6b1d92da2d72
Muhammed Javad Kasaragod <javadjazz899@gmail.com>
2024-07-09 12:23:36 +0530

    Interface and thier compailer

---
+++ src/config/Interface/Objects/IBaseUser.ts
@@ -0,0 +1,15 @@
+interface IBaseUser {
+    first_name: string
+    last_name: string
+    phone_number: string
+    email: string
+    auth_id: string
+    auth_provider: string
+    location: string
+    user_id?: string
+    profile_id?: string
+}
+
+
+
+export default IBaseUser
