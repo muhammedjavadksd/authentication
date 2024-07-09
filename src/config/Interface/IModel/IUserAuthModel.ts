41bb1fb719be367e3a573c77bb34fd7ae7c882d2
Muhammed Javad Kasaragod <javadjazz899@gmail.com>
2024-07-09 13:31:45 +0530

    UserAuth model interface created

---
+++ src/config/Interface/IModel/IUserAuthModel.ts
@@ -0,0 +1,24 @@
+import { Document } from "mongoose";
+
+interface locationTypeInterFace {
+    type: object,
+    coordinates: [number, number];
+}
+
+
+interface IUserModelDocument extends Document {
+    user_id: string;
+    phone_number: number;
+    email: string;
+    otp: number;
+    otp_timer: number;
+    auth_id?: string | null;
+    auth_provider: string;
+    first_name: string;
+    last_name: string;
+    jwtToken: string;
+    account_started?: boolean;
+    location?: locationTypeInterFace
+}
+
+export default IUserModelDocument
